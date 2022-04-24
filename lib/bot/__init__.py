5c53e3ba11c6243cc1e85155ce30362c61c1320a
swarnim38 <swarnim.marathe@gmail.com>
2022-04-24 14:57:57 +0530

    new embeds and bot version

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,6 +1,9 @@
 from discord import Intents
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import CommandNotFound
+from discord import Embed, File
+from datetime import datetime
 
 PREFIX = "o!"
 OWNER_IDS = [556801303595188227]
@@ -34,17 +37,51 @@ class Bot(BotBase):
     async def on_disconnect(self):
         print("Oasis disconnected")
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Something went wrong `0_0`")
+
+        else:
+            channel = self.get_channel(928595300828987445)
+            await channel.send("Something is wrong")
+
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+
+        elif hasattr(exc, "original"):
+            raise exc.original
+
+        else:
+            raise exc
+
     async def on_ready(self):
         if not self.ready:
             self.ready = True
             self.guild = self.get_guild(928595300828987442)
             print("Oasis is ready")
 
+            channel = self.get_channel(928595300828987445)
+            await channel.send("Oasis is formed!")
+
+            embed = Embed(title="Oasis is formed!",
+                          description="The bot is up and running",
+                          color=0Xf59e4c,
+                          timestamp=datetime.utcnow())
+
+            embed.set_footer(text="Chrysus")
+            embed.set_author(name="Oasis", icon_url=self.guild.icon_url)
+
+            await channel.send(embed=embed)
+            await channel.send(file=File("./data/images/oasis_banner.png"))
+
         else:
             print("Oasis recontructing...")
 
-    async def on_message(self, message):
-        pass
+            async def on_message(self, message):
+                pass
 
 
 bot = Bot()
